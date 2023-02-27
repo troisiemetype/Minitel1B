75e77cbaf43e94a598529399178b83b1b1faf709
3615 <internet-1@serandour.org>
2023-02-27 13:01:01 +0100

    Update Minitel1B_Hard.cpp

--- Minitel1B_Hard.cpp
+++ Minitel1B_Hard.cpp
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////
 /*
-   Minitel1B_Hard - Fichier source - Version du 27 février 2023 à 12h53
+   Minitel1B_Hard - Fichier source - Version du 27 février 2023 à 12h58
    Copyright 2016-2023 - Eric Sérandour
    https://entropie.org/3615/
    
