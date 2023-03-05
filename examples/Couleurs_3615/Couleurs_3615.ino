4a613299ac7fdb8826aeebc46a0ac9d373027a6f
3615 <internet-1@serandour.org>
2023-03-05 23:02:14 +0100

    Update Couleurs_3615.ino

--- examples/Couleurs_3615/Couleurs_3615.ino
+++ examples/Couleurs_3615/Couleurs_3615.ino
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////
 /*
-   3615 - Version du 5 mars 2023 à 22h43
+   3615 - Version du 5 mars 2023 à 23h00
    Copyright 2017-2023 - Eric Sérandour
    
    Attention : Ce programme fonctionne bien sur un ATMega 1284P.
@@ -107,6 +107,9 @@ void champVide(int premiereLigne, int nbLignes)
   }
   texte="";
   nbCaracteres=0;
+  for (int i=0; i<TAILLE_CACHE; i++) {
+    cache[i] = 0;
+  }
   minitel.moveCursorXY(31,24);
   minitel.print("puis ");
   minitel.attributs(INVERSION_FOND);
