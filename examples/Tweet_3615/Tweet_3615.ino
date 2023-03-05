31f1085745bb50ea825926e9074a3768cfc5cf3c
3615 <internet-1@serandour.org>
2023-03-05 23:58:09 +0100

    Update Tweet_3615.ino

--- examples/Tweet_3615/Tweet_3615.ino
+++ examples/Tweet_3615/Tweet_3615.ino
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////
 /*
-   3615 - Version du Version du 5 mars 2023 à 23h00
+   3615 - Version du Version du 5 mars 2023 à 23h58
    Copyright 2017-2023 - Eric Sérandour
    
    Documentation utilisée :
@@ -111,24 +111,31 @@ void champVide(int premiereLigne, int nbLignes)
 ////////////////////////////////////////////////////////////////////////
 
 void correction(int nbLignes) {
+  boolean texteCorrige = false;
   if ((nbCaracteres > 0) && (nbCaracteres <= 40*nbLignes)) {
-    if (nbCaracteres != 40*nbLignes) { minitel.moveCursorLeft(1); }
-    minitel.attributs(CARACTERE_BLEU);
-    minitel.print(VIDE);
-    minitel.attributs(CARACTERE_BLANC);
-    minitel.moveCursorLeft(1);
     unsigned int index = texte.length()-1;
     if (texte.charAt(index) >> 8) {  // Caractère spécial
-      texte = texte.substring(0,texte.length()-cache[0]);
-      for (int i=0; i<TAILLE_CACHE-1; i++) {
-        cache[i]=cache[i+1];
+      if (cache[0] != 0) {
+        texte = texte.substring(0,texte.length()-cache[0]);
+        texteCorrige = true;
+        for (int i=0; i<TAILLE_CACHE-1; i++) {
+          cache[i]=cache[i+1];
+        }
+        cache[TAILLE_CACHE-1] = 0;
       }
-      cache[TAILLE_CACHE-1] = 0;
     }
     else {
       texte = texte.substring(0,texte.length()-1);
+      texteCorrige = true;
     }
-    nbCaracteres--;
+    if (texteCorrige) {
+      nbCaracteres--;
+      if (nbCaracteres != 40*nbLignes) { minitel.moveCursorLeft(1); }
+      minitel.attributs(CARACTERE_BLEU);
+      minitel.print(VIDE);
+      minitel.attributs(CARACTERE_BLANC);
+      minitel.moveCursorLeft(1);
+    }
   }
 }
 
