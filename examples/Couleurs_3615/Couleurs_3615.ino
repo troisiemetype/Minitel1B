7db89db11c0bcb51f075d07f5ac07050c36bb27f
3615 <internet-1@serandour.org>
2023-03-06 18:00:24 +0100

    Update Couleurs_3615.ino

--- examples/Couleurs_3615/Couleurs_3615.ino
+++ examples/Couleurs_3615/Couleurs_3615.ino
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////
 /*
-   3615 Couleur - Version du 6 mars 2023 à 17h59
+   3615 Couleurs - Version du 6 mars 2023 à 17h59
    Copyright 2017-2023 - Eric Sérandour
    
    Attention : Ce programme fonctionne bien sur un ATMega 1284P.
