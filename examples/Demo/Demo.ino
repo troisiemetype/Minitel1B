2ebc464a3231f2935fb908bc54f1301183363b25
3615 <internet-1@serandour.org>
2023-03-17 14:45:47 +0100

    Update Demo.ino

--- examples/Demo/Demo.ino
+++ examples/Demo/Demo.ino
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////
 /*
-   Minitel1B_Hard - Démo - Version du 11 mars 2023 à 19h50
+   Minitel1B_Hard - Démo - Version du 17 mars 2023 à 14h45
    Copyright 2016, 2023 - Eric Sérandour
    https://entropie.org/3615/
    
@@ -39,8 +39,8 @@
 // Le troisième port série matériel de l'ESP32 (Serial2 / U2RXD U2TXD)
 // est utilisé pour la connexion avec le Minitel.
 Minitel minitel(Serial2);
-#else  // Pour ATmega 1284P notamment
-// Le deuxième port série matériel de l'ATMega 1284P (Serial1 / RXD1 TXD1)
+#else  // Pour ATmega 1284P ou ATmega 2560 notamment
+// Le deuxième port série matériel de l'ATMega (Serial1 / RXD1 TXD1)
 // est utilisé pour la connexion avec le Minitel.
 Minitel minitel(Serial1);
 #endif
@@ -50,7 +50,20 @@ int attente = 10000;
 ////////////////////////////////////////////////////////////////////////
 
 void setup() {
-  Serial.begin(9600);  // Port série matériel de l'ATmega à 9600 bauds.
+  // Le premier port série matériel de l'ATMega (Serial / RXD0 TXD0)
+  // ou de l'ESP32 (Serial / U0RXD U0TXD) est utilisé pour la connexion
+  // avec le PC.
+  Serial.begin(9600);
+
+  // Pour le cas où on utilise le shield 3615 avec une Arduino Mega 2560,
+  // on relie avec un fil les broches 8 et 19 d'un côté et 9 et 18 de l'autre.
+  // Les 2 broches 8 et 9 du shield 3615 sont dès lors respectivement reroutées
+  // vers les broches 19 (RX1) et 18 (TX1).
+  // Afin que les broches 8 et 9 de la carte Arduino ne perturbent pas
+  // ce reroutage, on les définit comme des entrées.
+  // pinMode(8,INPUT);
+  // pinMode(9,INPUT);
+
   minitel.changeSpeed(minitel.searchSpeed());
 }
 
